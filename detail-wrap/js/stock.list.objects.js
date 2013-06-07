3bae3f52236eaf236bbda0d69afc9f2abf069718
mactive <mactive@gmail.com>
2013-06-07 17:49:57 +0800

    backbone autocomplate

---
+++ detail-wrap/js/stock.list.objects.js
@@ -0,0 +1,20 @@
+var STOCK_LIST_OBJECTS = 
+[
+    {"symbol":"SZ000008","short":"BLL","name":"宝利来"},
+    {"symbol":"SZ000030","short":"FAGF","name":"富奥股份"},
+    {"symbol":"SZ000038","short":"SDT","name":"深大通"},
+    {"symbol":"SZ000068","short":"STHS","name":"ST华赛"},
+    {"symbol":"SZ000403","short":"STSH","name":"*ST生化"},
+    {"symbol":"SZ000408","short":"JGY","name":"金谷源"},
+    {"symbol":"SZ000409","short":"TFSY","name":"泰复实业"},
+    {"symbol":"SZ000420","short":"JLHX","name":"吉林化纤"},
+    {"symbol":"SZ000498","short":"SDLQ","name":"山东路桥"},
+    {"symbol":"SZ000505","short":"STZJ","name":"*ST珠江"},
+    {"symbol":"SZ000509","short":"SSTHS","name":"S*ST华塑"},
+    {"symbol":"SZ000520","short":"STFH","name":"*ST凤凰"},
+    {"symbol":"SZ000555","short":"STTG","name":"*ST太光"},
+    {"symbol":"SZ000576","short":"GDGH","name":"广东甘化"},
+    {"symbol":"SZ000585","short":"DBDQ","name":"东北电气"},
+    {"symbol":"SZ000594","short":"STGH","name":"*ST国恒"},
+    {"symbol":"SZ000595","short":"XBZC","name":"西北轴承"}
+];
